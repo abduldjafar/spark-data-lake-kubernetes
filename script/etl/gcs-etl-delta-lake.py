5cfcd402e3fc1fda61739029333ee1dafcc02aa7
kotekaman <kotekaman@kotekamans-MacBook-Pro.local>
2021-10-31 22:20:20 +0700

    clean code

--- script/etl/gcs-etl-delta-lake.py
+++ script/etl/gcs-etl-delta-lake.py
@@ -2,13 +2,12 @@ from delta import configure_spark_with_delta_pip
 import pyspark
 from configparser import ConfigParser
 from os import environ
-from pyspark.sql.types import StructType,StructField,BooleanType,MapType,IntegerType
-from pyspark.sql.functions import col,from_json,lit,round
-import json
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import col, lit, round
 
 # initialize config
 config = ConfigParser()
-config.read('config/python/config.ini')
+config.read("config/python/config.ini")
 
 # get value from config file
 gcs_jars = config["spark"]["gcs-connector"]
@@ -25,15 +24,23 @@ environ["GOOGLE_APPLICATION_CREDENTIALS"] = config["gcpconfig"]["json"]
 
 
 # spark initialization
-builder = pyspark.sql.SparkSession.builder.appName(apps_name) \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.jars",gcs_jars) \
-    .config("spark.jars",delta_contrib_jars) \
-    .config("spark.delta.logStore.gs.impl","io.delta.storage.GCSLogStore")
+builder = (
+    pyspark.sql.SparkSession.builder.appName(apps_name)
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.jars", gcs_jars)
+    .config("spark.jars", delta_contrib_jars)
+    .config("spark.delta.logStore.gs.impl", "io.delta.storage.GCSLogStore")
+)
 
 spark = spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
-data = spark.read.option("header",True).option("quote", "\"").option("escape", "\"").option("multiLine", True).csv(data_sources)
+data = (
+    spark.read.option("header", True)
+    .option("quote", '"')
+    .option("escape", '"')
+    .option("multiLine", True)
+    .csv(data_sources)
+)
 json_datas = spark.read.json(json_data_sources)
 datas = None
 datas_json_for_data_lake = None
@@ -54,41 +61,68 @@ except:
     datas_json_for_data_lake = spark.read.format("delta").load(gcs_data_sources_ids)
 
 # ET Process
-videos_selected_field = datas.select("title","category_id","views","likes","dislikes")
-id_selected_field = datas_json_for_data_lake.select("id","snippet.*") \
-    .withColumnRenamed("title","category")
+videos_selected_field = datas.select(
+    "title", "category_id", "views", "likes", "dislikes"
+)
+id_selected_field = datas_json_for_data_lake.select(
+    "id", "snippet.*"
+).withColumnRenamed("title", "category")
 
-get_percentage = videos_selected_field \
-    .join(id_selected_field,videos_selected_field.category_id ==  id_selected_field.id,"inner") \
-    .select("title","category","views","likes","dislikes") \
-    .withColumn("likes",(col("likes")/col("views")*lit(100))).withColumnRenamed("likes","likes_percentage") \
-    .withColumn("likes_percentage",round(col('likes_percentage'),2)) \
-    .withColumn("dislikes",(col("dislikes")/col("views")*lit(100))).withColumnRenamed("dislikes","dislikes_percentage") \
-    .withColumn("dislikes_percentage",round(col('dislikes_percentage'),2)) \
-    .withColumn("not_voted_percentage",lit(100)-(col("likes_percentage")+col("dislikes_percentage"))) \
-    .withColumn("not_voted_percentage",round(col('not_voted_percentage'),2)) \
-    .withColumn("views",col("views").cast(IntegerType()))
+get_percentage = (
+    videos_selected_field.join(
+        id_selected_field,
+        videos_selected_field.category_id == id_selected_field.id,
+        "inner",
+    )
+    .select("title", "category", "views", "likes", "dislikes")
+    .withColumn("likes", (col("likes") / col("views") * lit(100)))
+    .withColumnRenamed("likes", "likes_percentage")
+    .withColumn("likes_percentage", round(col("likes_percentage"), 2))
+    .withColumn("dislikes", (col("dislikes") / col("views") * lit(100)))
+    .withColumnRenamed("dislikes", "dislikes_percentage")
+    .withColumn("dislikes_percentage", round(col("dislikes_percentage"), 2))
+    .withColumn(
+        "not_voted_percentage",
+        lit(100) - (col("likes_percentage") + col("dislikes_percentage")),
+    )
+    .withColumn("not_voted_percentage", round(col("not_voted_percentage"), 2))
+    .withColumn("views", col("views").cast(IntegerType()))
+)
 
 
-most_not_voted_film = get_percentage.distinct() \
-    .groupBy("title","category","likes_percentage","dislikes_percentage","not_voted_percentage") \
-    .sum('views') \
-    .withColumnRenamed("sum(views)","views") \
-    .sort(col("not_voted_percentage").desc(),col("views").desc())
+most_not_voted_film = (
+    get_percentage.distinct()
+    .groupBy(
+        "title",
+        "category",
+        "likes_percentage",
+        "dislikes_percentage",
+        "not_voted_percentage",
+    )
+    .sum("views")
+    .withColumnRenamed("sum(views)", "views")
+    .sort(col("not_voted_percentage").desc(), col("views").desc())
+)
 
 most_not_voted_film.show()
 
-most_dislikes_film = get_percentage.distinct() \
-    .sort(col("dislikes_percentage").desc())
+most_dislikes_film = get_percentage.distinct().sort(col("dislikes_percentage").desc())
 
 most_dislikes_film.show()
 
-most_likes_film = get_percentage.distinct() \
-    .sort(col("likes_percentage").desc(),col("views").desc())
+most_likes_film = get_percentage.distinct().sort(
+    col("likes_percentage").desc(), col("views").desc()
+)
 
 most_likes_film.show()
 
 # write data into GCS
-most_not_voted_film.limit(5).write.mode("overwrite").format("delta").save(gcs_data_destination+"/five_most_not_voted_film")
-most_dislikes_film.limit(5).write.mode("overwrite").format("delta").save(gcs_data_destination+"/five_most_dislikes_film")
-most_likes_film.limit(5).write.mode("overwrite").format("delta").save(gcs_data_destination+"/five_most_likes_film")
+most_not_voted_film.limit(5).write.mode("overwrite").format("delta").save(
+    gcs_data_destination + "/five_most_not_voted_film"
+)
+most_dislikes_film.limit(5).write.mode("overwrite").format("delta").save(
+    gcs_data_destination + "/five_most_dislikes_film"
+)
+most_likes_film.limit(5).write.mode("overwrite").format("delta").save(
+    gcs_data_destination + "/five_most_likes_film"
+)
